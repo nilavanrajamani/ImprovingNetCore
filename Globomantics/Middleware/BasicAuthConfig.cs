bbda3398d89c91a65de28f451ee4434da853bee1
Nilavan Anbumozhi <nilavanrajamani@gmail.com>
2019-07-30 08:51:00 +0530

    Middleware Filters

---
+++ Globomantics/Middleware/BasicAuthConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Middleware
+{
+    public class BasicAuthConfig
+    {
+        public void Configure(IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<BasicAuthMiddleware>();
+        }
+    }
+}
